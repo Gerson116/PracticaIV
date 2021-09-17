59d70cf879fcd4c44d3a958ac5ad9c0a64e8c33a
gersondevx <gerson.santosm3@gmail.com>
2021-09-16 21:51:33 -0400

    Primer Commit

---
+++ PracticaVI/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticaVI
+{
+    public static class Helper
+    {
+
+        public static List<int> Pares(this List<int> num)
+        {
+            return num.FindAll(r => (r % 2) == 0);
+        }
+    }
+}
---
+++ PracticaVI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticaVI
+{
+    delegate bool MiDelegado(int numero);
+    class Program
+    {
+        private static List<int> _listadoDeNumeros = new List<int>
+        {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listado de números");
+            _listadoDeNumeros.Pares().ForEach(r => Console.WriteLine(r));
+            Console.ReadKey();
+        }
+
+        public bool EsImPar(int num)
+        {
+            return (num % 2) != 0;
+        }
+
+        public double CalcularITBIS(double precio)
+        {
+            return (precio * 18) / 100;
+        }
+
+        public DateTime GetDate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
